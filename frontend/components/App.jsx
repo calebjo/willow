194690412b536a8996aacecc907ebed720b0ce5d
calebjo <caleb.ri.jones@gmail.com>
2021-12-30 16:38:28 -0500

    Fix bugs preventing render, reinstall router

---
+++ frontend/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route } from "react-router";
+
+const App = () => (
+  <div className="app">
+    <div className="top-nav">
+      <header>
+        <h1>Willow</h1>
+      </header>
+    </div>
+  </div>
+);
+
+export default App;
--- frontend/willow.jsx
+++ frontend/willow.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-import ReactDOM from "react";
+import ReactDOM from "react-dom";
+
+import Root from "./components/root"
+import configureStore from "./store/store"
 
 document.addEventListener("DOMContentLoaded", () => {
+    // BEGIN TESTING
+    const store = configureStore()
+    window.getState = store.getState
+    window.dispatch = store.dispatch
+    // END TESTING
     const root = document.getElementById("root")
-    ReactDOM.render(<Root />, root)
+    ReactDOM.render(<Root store={store}/>, root)
 })

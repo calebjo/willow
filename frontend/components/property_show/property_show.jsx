e11e34bdb69be1f72a6318830abebc1c8f39854d
calebjo <caleb.ri.jones@gmail.com>
2022-01-05 10:24:16 -0500

    Fix property listing overflow styles

--- frontend/components/property_show/property_show.jsx
+++ frontend/components/property_show/property_show.jsx
@@ -4,20 +4,17 @@ import PropertyDetail from "./property_detail";
 export default class PropertyShow extends React.Component {
     constructor(props){
         super(props)
-        console.log("Component is constructing....")
     }
 
     calcTimeOnWillow() {
+        // change time on willow text based on actual time on willow
     }
 
     componentDidMount() {
         this.props.fetchProperties()
-        console.log("Component has mounted!")
     }
 
     render() {
-        console.log("Component is rendering....")
-        console.log(this.props)
         const properties = this.props.properties.map((property, idx) => {
             if (property.id) {
                 return (
@@ -26,12 +23,14 @@ export default class PropertyShow extends React.Component {
             }
         })
         return(
-            <div className="property-list-container">
-                <div className="property-header">
-                    {/* check if rental or for sale, render accordingly */}
-                </div>
-                <div className="property-list">
-                    { properties }
+            <div className="property-list-wrapper">
+                <div className="property-list-container">
+                    <div className="property-header">
+                        {/* check if rental or for sale, render accordingly */}
+                    </div>
+                    <div className="property-list">
+                        { properties }
+                    </div>
                 </div>
             </div>
         );

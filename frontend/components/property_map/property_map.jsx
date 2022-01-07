3650bba988d4d00027ea18c0d210921c71636ca5
calebjo <caleb.ri.jones@gmail.com>
2022-01-06 20:27:33 -0500

    Add Google Maps API integration

--- frontend/components/property_map/property_map.jsx
+++ frontend/components/property_map/property_map.jsx
@@ -6,9 +6,19 @@ export default class PropertyMap extends React.Component {
         super(props)
     }
 
+    componentDidMount(){
+        const mapOptions = {
+            center: { lat: 37.7758, lng: -122.435 }, // this is SF
+            zoom: 13
+        };
+        const map = this.refs.map;
+        
+        this.map = new google.maps.Map(map, mapOptions);
+    }
+
     render() {
         return(
-            <div className="property-map">
+            <div className="property-map" ref="map">
             </div>
         );
     }

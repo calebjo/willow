ca92b7d6780511cb49b412fcc6dff630f8692368
calebjo <caleb.ri.jones@gmail.com>
2022-01-06 22:49:38 -0500

    Fix splash page link card types

--- frontend/components/link/link_card.jsx
+++ frontend/components/link/link_card.jsx
@@ -10,36 +10,32 @@ export default class LinkCard extends React.Component {
         this.copy = null;
         this.buttonText = null;
         this.setCardContent = this.setCardContent.bind(this)
+        this.setCardContent()
     }
     
     setCardContent(){
-        // console.log(this.props)
-        switch(this.props.type){
-            case ("buy"):
-                this.image = window.buyHome
-                this.title = "Buy a home"
-                this.copy = "Find your place with an immersive photo experience and the most listings, including things you won't find anywhere else."
-                this.buttonText = "Search homes"
-                // console.log("Rendering a buy card")
-            case ("rent"):
-                this.image = window.rentHome
-                this.title = "Sell a home"
-                this.copy = "No matter what path you take to sell your home, we can help you navigate a successful sale."
-                this.buttonText = "See your options"
-                // console.log("Rendering a rent card")
-            case ("sell"):
-                this.image = window.sellHome
-                this.title = "Rent a home"
-                this.copy = "We're creating a seamless online experience - from shopping on the largest rental network, to applying, to paying rent."
-                this.buttonText = "Find rentals"
-                // console.log("Rendering a sell card")
-            default:
-                // console.log("There's an error in the card component.")
+        if (this.props.type === "buy") {
+            this.image = window.buyHome
+            this.title = "Buy a home"
+            this.copy = "Find your place with an immersive photo experience and the most listings, including things you won't find anywhere else."
+            this.buttonText = "Search homes"
+            console.log("Rendering a buy card")
+        } else if (this.props.type === "rent") {
+            this.image = window.rentHome
+            this.title = "Sell a home"
+            this.copy = "No matter what path you take to sell your home, we can help you navigate a successful sale."
+            this.buttonText = "See your options"
+            console.log("Rendering a rent card")
+        } else if (this.props.type === "sell") {
+            this.image = window.sellHome
+            this.title = "Rent a home"
+            this.copy = "We're creating a seamless online experience - from shopping on the largest rental network, to applying, to paying rent."
+            this.buttonText = "Find rentals"
+            console.log("Rendering a sell card")
         }
     }
 
     render() {
-        this.setCardContent()
         return(
             <div className="link-card">
                 <div className="card-image">

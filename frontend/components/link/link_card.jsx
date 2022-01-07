0ee1a3c3c4d3c89dca18c849c29547cacc1dc5a0
calebjo <caleb.ri.jones@gmail.com>
2022-01-06 23:26:44 -0500

    Refactor splash link cards to actually be links, adjust styling

--- frontend/components/link/link_card.jsx
+++ frontend/components/link/link_card.jsx
@@ -1,51 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-// SKELETON
 export default class LinkCard extends React.Component {
     constructor(props){
         super(props)
         this.state = { type: null }
+        this.linkUrl = "/";
         this.image = null;
         this.title = null;
         this.copy = null;
         this.buttonText = null;
+
         this.setCardContent = this.setCardContent.bind(this)
         this.setCardContent()
-    }
-
-    handleMouseLeave(e) {
-        e.target.classList.remove("focused")
-    }
-
-    handleMouseEnter(e) {
-        e.target.classList.add("focused")
     }
     
     setCardContent(){
         if (this.props.type === "buy") {
+            this.linkUrl = "/homes"
             this.image = window.buyHome
             this.title = "Buy a home"
             this.copy = "Find your place with an immersive photo experience and the most listings, including things you won't find anywhere else."
             this.buttonText = "Search homes"
-            console.log("Rendering a buy card")
-        } else if (this.props.type === "rent") {
-            this.image = window.rentHome
+        } else if (this.props.type === "sell") {
+            this.linkUrl = "/sell"
+            this.image = window.sellHome
             this.title = "Sell a home"
             this.copy = "No matter what path you take to sell your home, we can help you navigate a successful sale."
             this.buttonText = "See your options"
-            console.log("Rendering a rent card")
-        } else if (this.props.type === "sell") {
-            this.image = window.sellHome
+        } else if (this.props.type === "rent") {
+            this.linkUrl = "/homes/for_rent"
+            this.image = window.rentHome
             this.title = "Rent a home"
             this.copy = "We're creating a seamless online experience - from shopping on the largest rental network, to applying, to paying rent."
             this.buttonText = "Find rentals"
-            console.log("Rendering a sell card")
         }
     }
 
     render() {
         return(
-            <div className="link-card">
+            <Link to={ this.linkUrl } >
                 <div className="card-image">
                     <img src={this.image} />
                 </div>
@@ -58,7 +52,7 @@ export default class LinkCard extends React.Component {
                 <div className="card-button">
                     {this.buttonText}
                 </div>
-            </div>
+            </Link>
         );
     }
 }

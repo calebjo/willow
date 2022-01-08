a9a0aa1ce456dbf6f2618273936689525fd48104
calebjo <caleb.ri.jones@gmail.com>
2022-01-08 01:52:24 -0500

    Add basic layout and styling for sell form page

--- frontend/components/pages/sell_form_content.jsx
+++ frontend/components/pages/sell_form_content.jsx
@@ -17,6 +17,36 @@ export default class SellFormContent extends React.Component {
         return (
             <div className="sell-form-content">
                 <TopNavContainer />
+                <div className="sell-form-banner">
+                    <div className="sell-form-container">
+
+                    </div>
+                </div>
+                <div className="sell-form-mid">
+                    <div className="sell-form-header">
+                        Why post on Willow?
+                    </div>
+                    <div className="sell-form-cards">
+                        <div className="sell-form-card">
+                            <img src={window.sellFormHouse} />
+                            <div className="sell-form-card-copy">
+                                Post a listing for free, including up to eight photos.
+                            </div>
+                        </div>
+                        <div className="sell-form-card">
+                            <img src={window.sellFormVolume} />
+                            <div className="sell-form-card-copy">
+                                Your home will be listed on Willow, reaching the "largest" audience of shoppers on the Web.
+                            </div>
+                        </div>
+                        <div className="sell-form-card">
+                            <img src={window.sellFormCheck} />
+                            <div className="sell-form-card-copy">
+                                Home shoppers can search for your new listing on Willow.
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <Footer />
             </div>
         )

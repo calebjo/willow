dc07f8d27e0412277ce2abfb0bb1a4c0b849122f
calebjo <caleb.ri.jones@gmail.com>
2022-01-10 19:54:38 -0500

    Add bounds calculation in properties model

--- app/models/property.rb
+++ app/models/property.rb
@@ -5,5 +5,12 @@ class Property < ApplicationRecord
     # Active Storage Association
     #has_many_attached :photos
     has_one_attached :photo
+    
+    def self.in_bounds(bounds)
+        self.where("lat < ?", bounds[:northEast][:lat])
+            .where("lat > ?", bounds[:southWest][:lat])
+            .where("lng > ?", bounds[:southWest][:lng])
+            .where("lng < ?", bounds[:northEast][:lng])
+    end
 end
   
